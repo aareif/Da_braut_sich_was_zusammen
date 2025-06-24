78de2d314b7f891308a725ab951e46495d892b9c
dankol187 <dankol@schuldorf.de>
2025-06-24 09:20:34 +0200

    Update nutzer_login.php

--- nutzer_login.php
+++ nutzer_login.php
@@ -49,13 +49,14 @@ $db->disconnect();
         overflow: hidden;
     }
 
+    /* Sonne */
     .sun {
         position: absolute;
         top: 40px;
         left: 50%;
         transform: translateX(-50%);
-        width: 80px;
-        height: 80px;
+        width: 100px;
+        height: 100px;
         background: #ffe066;
         box-shadow: inset 0 0 10px #fffab3, 0 0 20px #ffd700;
         animation: sunMove 8s ease-in-out infinite alternate;
@@ -66,6 +67,7 @@ $db->disconnect();
         100% { transform: translateX(-50%) translateY(20px); }
     }
 
+    /* Wolken */
     .cloud {
         position: absolute;
         width: 100px;
@@ -76,65 +78,35 @@ $db->disconnect();
             40px 0 #fff,
             60px 10px #fff,
             80px 0 #fff;
-        animation: cloudMove linear infinite;
+        animation: cloudDrift 60s ease-in-out infinite alternate;
     }
 
-    /* Cloud Variationen */
-    .cloud1 {
-        top: 50px;
-        left: -150px;
-        transform: scale(1);
-        animation-duration: 80s;
+    @keyframes cloudDrift {
+        0% { transform: translateX(0); }
+        100% { transform: translateX(20px); }
     }
 
-    .cloud2 {
-        top: 100px;
-        left: -250px;
-        transform: scale(0.9);
-        animation-duration: 100s;
-    }
+    .cloud1 { top: 40px; left: 10%; transform: scale(1); z-index: 1; }
+    .cloud2 { top: 90px; left: 30%; transform: scale(0.9); z-index: 2; }
+    .cloud3 { top: 60px; left: 55%; transform: scale(1.1); z-index: 1; }
+    .cloud4 { top: 120px; left: 70%; transform: scale(0.8); z-index: 2; }
+    .cloud5 { top: 30px; left: 85%; transform: scale(1.2); z-index: 1; }
 
-    .cloud3 {
-        top: 150px;
-        left: -200px;
-        transform: scale(1.1);
-        animation-duration: 70s;
-    }
-
-    .cloud4 {
-        top: 70px;
-        left: -300px;
-        transform: scale(0.8);
-        animation-duration: 90s;
-    }
-
-    .cloud5 {
-        top: 180px;
-        left: -100px;
-        transform: scale(1.2);
-        animation-duration: 110s;
-    }
-
-    @keyframes cloudMove {
-        0% { left: -200px; }
-        100% { left: 110%; }
-    }
-
+    /* Grasblock */
     .container {
         position: relative;
-        top: 200px;
+        top: 180px;
         margin: 0 auto;
-        width: 380px;
+        width: 420px;
         padding: 0;
         border: 4px solid #3c2f1f;
         box-shadow: 0 0 20px #000;
-        border-radius: 4px;
         text-align: center;
         background: linear-gradient(
             to bottom,
             #3fa23f 0px,
-            #3fa23f 50px,
-            #865f3a 50px,
+            #3fa23f 30px,
+            #865f3a 30px,
             #865f3a 100%
         );
     }
@@ -180,9 +152,10 @@ $db->disconnect();
         margin-bottom: 15px;
     }
 
+    /* Sand und Meer */
     .sand {
         position: absolute;
-        bottom: 100px;
+        bottom: 0;
         width: 100%;
         height: 50px;
         background: repeating-linear-gradient(
@@ -192,11 +165,12 @@ $db->disconnect();
             #e5c37f 10px,
             #e5c37f 20px
         );
+        z-index: 0;
     }
 
     .ocean {
         position: absolute;
-        bottom: 0;
+        bottom: 50px;
         width: 100%;
         height: 100px;
         background: repeating-linear-gradient(
@@ -207,6 +181,7 @@ $db->disconnect();
             #33bbff 20px
         );
         animation: wave 8s ease-in-out infinite alternate;
+        z-index: 0;
     }
 
     @keyframes wave {
@@ -220,7 +195,7 @@ $db->disconnect();
 <!-- Sonne -->
 <div class="sun"></div>
 
-<!-- Wolken in verschiedenen Höhen -->
+<!-- Wolken -->
 <div class="cloud cloud1"></div>
 <div class="cloud cloud2"></div>
 <div class="cloud cloud3"></div>
@@ -246,9 +221,9 @@ $db->disconnect();
     </form>
 </div>
 
-<!-- Strand und Meer -->
-<div class="sand"></div>
+<!-- Landschaft -->
 <div class="ocean"></div>
+<div class="sand"></div>
 
 </body>
 </html>
